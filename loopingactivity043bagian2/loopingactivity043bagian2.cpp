cde9232fdf56ef8a60fedc6cefecb38b038a449e
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-03-22 17:38:59 +0700

    selesai bang

---
+++ loopingactivity043bagian2/loopingactivity043bagian2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a[5];
+	cout << "Mengisi Array" << endl;
+	for (int i = 0; i < 5; i++) {
+		cout << "data ke- " << (i + 1) << ": ";
+		cin >> a[i];
+	}
+	cout << "Membaca Array" << endl;
+	for (int i = 0; i < 5; i++) {
+		cout << "data ke-" << (i + 1) << a[i] << endl;
+	}
+
+}
